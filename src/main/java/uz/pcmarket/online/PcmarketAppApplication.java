965a49e321d90a2eed60a46a878947fe5a6bf2b4
farrukhgulomov <frankywht@gmail.com>
2023-10-12 07:01:42 +0500

    creating ecommerce online shop computers

---
+++ src/main/java/uz/pcmarket/online/PcmarketAppApplication.java
@@ -0,0 +1,13 @@
+package uz.pcmarket.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcmarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcmarketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pcmarket/online/PcmarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pcmarket.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
